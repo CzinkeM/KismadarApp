7aef57311ec41ab78af38c099e3e5cd2387298b1
Czinke Márton <czinke.marci@gmail.com>
2021-02-23 23:01:34 +0100

    Refactor XML codes

--- app/src/main/java/com/example/kismadrapp/fragments/TownDetailFragment.kt
+++ app/src/main/java/com/example/kismadrapp/fragments/TownDetailFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.kismadrapp.R
+import com.example.kismadrapp.databinding.FragmentDetailsTownBinding
 
 class TownDetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details_town, container, false)
+    ): View {
+        val binding: FragmentDetailsTownBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_details_town,container,false)
+
+        return binding.root
     }
 }
