008ac822e76c58d8f2ee0acab8b84d116f5a57d0
Czinke Márton <czinke.marci@gmail.com>
2021-02-28 02:08:25 +0100

    Implement open contact in their apps

--- app/src/main/java/com/example/kismadrapp/utils/Utils.kt
+++ app/src/main/java/com/example/kismadrapp/utils/Utils.kt
@@ -1,10 +1,15 @@
 package com.example.kismadrapp.utils
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.core.content.res.ResourcesCompat
 import com.example.kismadrapp.R
 import com.example.kismadrapp.models.*
+import java.lang.Exception
+import javax.security.auth.Subject
 import kotlin.random.Random
 
 enum class NatureType { LAKE, VIEWPOINT, SPRING, OTHER }
@@ -223,6 +228,23 @@ fun getSights(res: Resources): ArrayList<Sight>{
         R.drawable.vector_sight,null)!!,null)
     return arrayListOf(testSight)
 }
-fun getCategoryAsCategoryModel(){
-
+fun openFacebook(packageManager: PackageManager,url: String): Intent{
+    var uri = Uri.parse(url)
+    val facebookAppName = "com.facebook.katana"
+    try {
+        val applicationInfo = packageManager.getApplicationInfo(facebookAppName,0)
+        if(applicationInfo.enabled){
+            uri = Uri.parse("fb://facewebmodal/f?href=$url")
+        }
+    }catch (e: PackageManager.NameNotFoundException){}
+    return Intent(Intent.ACTION_VIEW,uri)
+}
+fun openWebsite(url: String): Intent{
+    return Intent(Intent.ACTION_VIEW, Uri.parse(url))
+}
+fun openEmail(email: String,subject: String):Intent{
+    val intent = Intent(Intent.ACTION_SENDTO)
+    intent.data = Uri.parse("mailto:$email")
+    intent.putExtra(Intent.EXTRA_SUBJECT,subject)
+    return intent
 }
