868ebcbb78b89d96d2b32396bf3a4c8cb6857849
Czinke Márton <czinke.marci@gmail.com>
2021-03-01 20:49:06 +0100

    Clean up WelcomeActivity and its ViewModel

--- app/src/main/java/com/example/kismadrapp/activities/WelcomeActivity.kt
+++ app/src/main/java/com/example/kismadrapp/activities/WelcomeActivity.kt
@@ -1,23 +1,19 @@
 package com.example.kismadrapp.activities
 
 import android.app.Activity
-import android.app.Dialog
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
-import android.view.View
-import android.widget.Button
+import android.view.LayoutInflater
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.observe
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kismadrapp.R
@@ -29,7 +25,6 @@ import com.example.kismadrapp.viewmodels.WelcomeActivityViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.zxing.integration.android.IntentIntegrator
 import org.json.JSONObject
 import java.net.URL
@@ -38,8 +33,8 @@ import java.net.URL
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var viewModel: WelcomeActivityViewModel
-    lateinit var navigationView: NavigationView
-    private lateinit var dialog: Dialog
+    lateinit var drawerNavigationView: NavigationView
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     private var _weather = MutableLiveData<Weather>()
     val weather: LiveData<Weather>
@@ -49,32 +44,23 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_KismadárApp)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
         WeatherTask().execute()
         viewModel = ViewModelProvider(this).get(WelcomeActivityViewModel::class.java)
         drawerLayout = findViewById(R.id.drawerLayout)
-        navigationView = findViewById(R.id.navView)
-        navigationView.setupWithNavController(findNavController(R.id.fragment))
-        viewModel.readFromDataStore.observe(this) { myName ->
-            Log.i("datastore", myName)
-        }
-        Log.i("DataStore", "${viewModel.readFromDataStore.value} value")
-        setMenuItemClickListener(navigationView)
-
-        supportActionBar?.hide()
-        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navController = findNavController(R.id.fragment)
-        bottomNavigation.setupWithNavController(navController)
-        navigationView.setupWithNavController(navController)
+        drawerNavigationView = findViewById(R.id.navView)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
         val bottomNavigationQRCodeButton = findViewById<BottomNavigationItemView>(R.id.bottomNavQr)
         val bottomNavigationOpenDrawerButton = findViewById<BottomNavigationItemView>(R.id.bottomNavSettings)
+        val navController = findNavController(R.id.fragment)
+        bottomNavigationView.setupWithNavController(navController)
+        drawerNavigationView.setupWithNavController(navController)
+        drawerNavigationView.setNavigationItemSelectedListener(setMenuItemClickListener())
+
         bottomNavigationQRCodeButton.setOnClickListener {
-            val scanner = IntentIntegrator(this)
-            scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES)
-            scanner.setBeepEnabled(false)
-            scanner.initiateScan()
+            viewModel.openQrCodeReader(this)
         }
         bottomNavigationOpenDrawerButton.setOnClickListener {
-            bottomNavigation[0].isActivated
             this.openDrawer()
         }
     }
@@ -82,59 +68,12 @@ class WelcomeActivity : AppCompatActivity() {
     fun openDrawer() {
         drawerLayout.openDrawer(Gravity.LEFT)
     }
-
-    private fun createDialogWindow(itemId: Int, viewModel: WelcomeActivityViewModel) {
-        val dialogBuilder: AlertDialog.Builder
-        val languagePopupView: View
-        val closeButton: Button
-
-        val switchLangHun: SwitchMaterial
-        val switchLangEng: SwitchMaterial
-        when (itemId) {
-            R.id.menuItemLanguage -> {
-                dialogBuilder = AlertDialog.Builder(this)
-                languagePopupView = layoutInflater.inflate(R.layout.layout_setting_language, null)
-                dialogBuilder.setView(languagePopupView)
-                closeButton = languagePopupView.findViewById(R.id.settingLanguageClose)
-                switchLangEng = languagePopupView.findViewById(R.id.switchEng)
-                switchLangHun = languagePopupView.findViewById(R.id.switchHun)
-                viewModel.checkAndSetSwitchState(switchLangHun, switchLangEng)
-                dialog = dialogBuilder.create()
-                dialog.show()
-                closeButton.setOnClickListener {
-                    Toast.makeText(this, "Close", Toast.LENGTH_SHORT).show()
-                    viewModel.saveToDataStore(resources.getString(R.string.key_language_en))
-                    dialog.dismiss()
-                }
-            }
-            R.id.menuItemAbout -> {
-                dialogBuilder = AlertDialog.Builder(this)
-                languagePopupView = layoutInflater.inflate(R.layout.layout_setting_about, null)
-                dialogBuilder.setView(languagePopupView)
-                closeButton = languagePopupView.findViewById(R.id.settingAboutClose)
-                dialog = dialogBuilder.create()
-                dialog.show()
-                closeButton.setOnClickListener {
-                    Toast.makeText(this, "Close", Toast.LENGTH_SHORT).show()
-                    dialog.dismiss()
-                }
-            }
-        }
-    }
-
-    private fun setMenuItemClickListener(navigationView: NavigationView) {
-        // TODO: 2021. 02. 24. Cserélni a recyclerViewra a navigationt
-        navigationView.setNavigationItemSelectedListener { item ->
+    private fun setMenuItemClickListener(): NavigationView.OnNavigationItemSelectedListener {
+        return NavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menuItemAbout -> {
-                    createDialogWindow(item.itemId, viewModel)
-                    Toast.makeText(this, "about", Toast.LENGTH_SHORT).show()
-                    return@setNavigationItemSelectedListener true
-                }
-                R.id.menuItemLanguage -> {
-                    createDialogWindow(item.itemId, viewModel)
-                    Toast.makeText(this, "language", Toast.LENGTH_SHORT).show()
-                    return@setNavigationItemSelectedListener true
+                    viewModel.createDialogWindow(item.itemId, layoutInflater,this)
+                    true
                 }
                 R.id.menuContactEmail -> {
                     startActivity(openEmail(getString(R.string.emailAddress), "Applikáció"))
@@ -145,19 +84,15 @@ class WelcomeActivity : AppCompatActivity() {
                     true
                 }
                 R.id.menuContactWebsite -> {
-                    startActivity(openWebsite(getString(R.string.websiteUrl)))
+                    startActivity(openWebsite(getString(R.string.websiteUrl)),null)
                     true
                 }
-
-                else -> {
-                    Toast.makeText(this, "Else happened", Toast.LENGTH_SHORT).show()
-                    return@setNavigationItemSelectedListener true
-                }
+                else -> true
             }
         }
     }
 
-    inner class WeatherTask: AsyncTask<String, Void, String>(){
+    private inner class WeatherTask: AsyncTask<String, Void, String>(){
         override fun onPreExecute() {
             super.onPreExecute()
         }
--- app/src/main/java/com/example/kismadrapp/viewmodels/WelcomeActivityViewModel.kt
+++ app/src/main/java/com/example/kismadrapp/viewmodels/WelcomeActivityViewModel.kt
@@ -1,27 +1,51 @@
 package com.example.kismadrapp.viewmodels
 
+import android.app.Activity
 import android.app.Application
+import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.kismadrapp.R
+import com.example.kismadrapp.utils.openEmail
+import com.example.kismadrapp.utils.openFacebook
+import com.example.kismadrapp.utils.openWebsite
 import com.example.kismadrapp.utils.repository.DataStoreRepository
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class WelcomeActivityViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository = DataStoreRepository(application)
-    private val context = application.applicationContext
 
-    val readFromDataStore = repository.readFromDataStore.asLiveData()
-
-    fun saveToDataStore(name: String) = viewModelScope.launch(Dispatchers.IO) {
-        repository.saveToDataStore(name)
+    fun createDialogWindow(itemId: Int, layoutInflater: LayoutInflater, context: Context) {
+        when (itemId) {
+            R.id.menuItemAbout -> {
+                val dialogBuilder = AlertDialog.Builder(context)
+                val languagePopupView = layoutInflater.inflate(R.layout.layout_setting_about, null)
+                dialogBuilder.setView(languagePopupView)
+                val closeButton = languagePopupView.findViewById<Button>(R.id.settingAboutClose)
+                val dialog = dialogBuilder.create()
+                dialog.show()
+                closeButton.setOnClickListener {
+                    Toast.makeText(context, "Close", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+            }
+        }
     }
-
-    fun checkAndSetSwitchState(switchHU: SwitchMaterial, switchEN: SwitchMaterial) {
-
+    fun openQrCodeReader(activity: Activity){
+        val scanner = IntentIntegrator(activity)
+        scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES)
+        scanner.setBeepEnabled(false)
+        scanner.initiateScan()
     }
-
-
 }
